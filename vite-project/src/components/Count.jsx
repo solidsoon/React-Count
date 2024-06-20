c9c42db21797db3fab31afddf7f7586ef45d1e2c
Kauan <162659253+solidsoon@users.noreply.github.com>
2024-06-20 20:04:45 -0300

    commit

--- vite-project/src/components/Count.jsx
+++ vite-project/src/components/Count.jsx
@@ -1,10 +1,10 @@
 import React, {useState} from "react";
 
 function Contador(){
-    const [contador, setContador] = useState(0); //<- inicia o array com o valor zero
+    const [contador, setContador] = useState(0); 
 
     const incrementar = () =>{
-        setContador(contador + 1); // funão de incrementar: contador tinha valor zero mas vai passar a aumentar de 1 em 1
+        setContador(contador + 1); 
     }
 
     const decrementar = () => {
@@ -16,8 +16,8 @@ function Contador(){
             <h2>Contador</h2>
             <h3>Escolha uma das opções abaixo!</h3>
             <p>Valor: {contador}</p>
-            <button className="btn" onClick={incrementar}>Incrementar</button>
-            <button className="btn" onClick={decrementar}>Decrementar</button>
+            <button className="btn" onClick={incrementar}>Aumentar</button>
+            <button className="btn" onClick={decrementar}>Diminuir</button>
         </div>
     );
 }
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Contador from './components/Count'
+import Count from './components/Count'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Contador />
+    <Count />
   </React.StrictMode>,
 )
